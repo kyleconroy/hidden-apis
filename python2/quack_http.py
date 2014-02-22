b0ee1fee75c9bf76d394a18b04a04f8c34bd31a0
Kyle Conroy <kyle@kyleconroy.com>
2014-02-22 01:02:04 -0800

    With MonkeyPatching, urllib2 works

--- python2/quack_http.py
+++ python2/quack_http.py
@@ -4,8 +4,15 @@ import json
 import socks
 import urllib2
 
-socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", 9150)
+def create_connection(address, timeout=None, source_address=None):
+    sock = socks.socksocket()
+    sock.connect(address)
+    return sock
+
+# Gotta' Monkey Patch 'Em Al
+socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", 9150, True)
 socket.socket = socks.socksocket
+socket.create_connection = create_connection
 
 resp = urllib2.urlopen("http://3g2upl4pq6kufc4m.onion/?q=define+ostensibly&format=json")
 
